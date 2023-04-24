e959e18843b824656e697c4a4f3c72180d2dd2bb
diegohy-sk~ <diegohy@al.insper.edu.br>
2023-04-24 14:08:54 -0300

    primeiro git

--- jogo/jogo.py
+++ jogo/jogo.py
@@ -1,3 +1,26 @@
-vasco = 1
+import pygame
+from telas import *
 
-corinthians = 2
+
+class Jogo:
+    def __init__(self):
+        pygame.init()
+        self.largura_janela = 1280
+        self.altura_janela = 720
+        self.window = pygame.display.set_mode((self.largura_janela, self.altura_janela))
+
+        self.telas = [TelaMenu(self.largura_janela,self.altura_janela)]
+        self.ind_tela_atual = 0
+
+
+    def roda(self):
+
+        tela_atual = self.telas[self.ind_tela_atual]
+
+        while tela_atual.atualiza():
+            tela_atual.desenha(self.window)
+
+        pygame.quit()
+
+if True:
+    Jogo().roda()
---
+++ jogo/telas.py
@@ -0,0 +1,41 @@
+import pygame
+
+class TelaMenu:
+    def __init__(self, largura_janela, altura_janela):
+        self.largura_janela = largura_janela
+        self.altura_janela = altura_janela
+
+        self.TITLE = pygame.image.load('assets/bomber_title.png')
+        self.TITLE_w, self.TITLE_h = self.TITLE.get_size()
+        self.TITLE_x, self.TITLE_y = (self.largura_janela - self.TITLE_w) / 2, 0
+
+        self.font = pygame.font.get_default_font()
+
+        self.START = pygame.font.Font(self.font, 60).render('START', True, (255, 255, 255))
+        self.START_w, self.START_h = self.START.get_size()
+        self.START_x, self.START_y = (self.largura_janela - self.START.get_width()) / 2, self.altura_janela / 2
+
+    def desenha(self, window):
+        window.fill((0, 100, 0))
+        window.blit(self.TITLE, (self.TITLE_x, self.TITLE_y))
+        window.blit(self.START, (self.START_x, self.START_y))
+        pygame.display.update()
+
+    def atualiza(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return False
+            if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                pass
+
+        return True
+    
+class TelaJogo:
+    def __init__(self, largura_janela, altura_janela):
+        self.largura_janela = largura_janela
+        self.altura_janela = altura_janela
+
+        self.bloco_inquebravel = pygame.image.load('assets/bloco_inquebravel.png')
+        self.bloco_quebravel = pygame.image.load('assets/bloco_quebravel.png')
+    
