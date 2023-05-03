e06ecf37dfa1519371a737b423c97845450ad393
Daniel Prado <daniel.vascomadagama2@gmail.com>
2023-05-02 22:05:28 -0300

    .

--- jogo/jogo.py
+++ jogo/jogo.py
@@ -6,15 +6,15 @@ from telajogo import TelaJogo
 class Jogo:
     def __init__(self):
         pygame.init()
-        self.largura_janela = 1280
-        self.altura_janela = 720
+        self.largura_janela = 1920
+        self.altura_janela = 1080
         self.window = pygame.display.set_mode((self.largura_janela, self.altura_janela))
         self.melhor_de_ = 0  # esta variavel é referente modalidade do jogo, melhor de 1, 3 ou 5
         self.vitorias_branco = 0
         self.vitorias_preto = 0
 
     def roda(self):
-        tela_atual = TelaMenu(self.largura_janela, self.altura_janela)
+        tela_atual = TelaJogo(self.largura_janela, self.altura_janela)
 
         rodando = True
         while rodando:
